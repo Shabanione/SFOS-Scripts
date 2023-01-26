3304c93a000c1746b58fe1431d50cf366a8cb5ec
Shabanione <hirvi@zaphod.ch>
2023-01-26 14:47:17 +0100

    Update quickdebug.sh
    Little bit cleanup
    changed display order little bit

--- quickdebug.sh
+++ quickdebug.sh
@@ -6,9 +6,9 @@
 #
 # Usage: sh ./quickdebug.sh
 #
-echo -e "\n" >quickdebug.txt
 cish -c "show date" >>quickdebug.txt
-echo -e "\n" >>quickdebug.txt
+echo "# Version Info" >>quickdebug.txt
+cish -c "system diagnostics show version-info" >verinf.txt && cat verinf.txt >>quickdebug.txt
 echo "# Routing and SD-WAN Settings"  >>quickdebug.txt
 cish -c "system route_precedence show" >>quickdebug.txt
 cish -c "show routing sd-wan-policy-route reply-packet" >>quickdebug.txt
@@ -17,22 +17,14 @@ cish -c "show routing reroute-connection" >>quickdebug.txt
 cish -c "show routing reroute-snat-connection" >>quickdebug.txt
 cish -c "show routing source-base-route-for-alias" >>quickdebug.txt
 cish -c "show routing dgd-probe-delay" >>quickdebug.txt
-echo -e "\n" >>quickdebug.txt
 echo "# Settings prone to intermittent network interruptions" >>quickdebug.txt
 connremovestat=$(cish -c "show vpn conn-remove-tunnel-up") && echo "vpn conn-remove-tunnel-up is $connremovestat" >>quickdebug.txt
-echo -e "\n" >>quickdebug.txt
 echo "# (un)loaded system modules" >>quickdebug.txt
 cish -c "system system_modules show" >>quickdebug.txt
-echo -e "\n" >>quickdebug.txt
-echo "# IPS settings" >>quickdebug.txt
-cish -c "show ips-settings" >>quickdebug.txt
-echo -e "\n" >>quickdebug.txt
 echo "# Firewall General Setting"  >>quickdebug.txt
 cish -c "system firewall-acceleration show" >>quickdebug.txt
 cish -c "system auto-reboot-on-hang show" >>quickdebug.txt
 cish -c "show advanced-firewall" >>quickdebug.txt
-echo -e "\n" >>quickdebug.txt
-echo "# Version Info" >>quickdebug.txt
-cish -c "system diagnostics show version-info" >verinf.txt && cat verinf.txt >>quickdebug.txt
-echo -e "\n" >>quickdebug.txt
+echo "# IPS settings" >>quickdebug.txt
+cish -c "show ips-settings" >>quickdebug.txt
 less quickdebug.txt
