f0abc0795437e017d05cf986a52eb9d4a0cd7b9a
Shabanione <hirvi@zaphod.ch>
2023-01-26 14:18:12 +0100

    Update quickdebug.sh
    Changed resulting filename to quickdebug.txt

--- quickdebug.sh
+++ quickdebug.sh
@@ -1,30 +1,30 @@
-echo -e "\n" >debugroute.txt
-cish -c "show date" >>debugroute.txt
-echo -e "\n" >>debugroute.txt
-echo "# Routing and SD-WAN Settings"  >>debugroute.txt
-cish -c "system route_precedence show" >>debugroute.txt
-cish -c "show routing sd-wan-policy-route reply-packet" >>debugroute.txt
-cish -c "show routing sd-wan-policy-route system-generate-traffic" >>debugroute.txt
-cish -c "show routing reroute-connection" >>debugroute.txt
-cish -c "show routing reroute-snat-connection" >>debugroute.txt
-cish -c "show routing source-base-route-for-alias" >>debugroute.txt
-cish -c "show routing dgd-probe-delay" >>debugroute.txt
-echo -e "\n" >>debugroute.txt
-echo "# Settings prone to intermittent network interruptions" >>debugroute.txt
-connremovestat=$(cish -c "show vpn conn-remove-tunnel-up") && echo "vpn conn-remove-tunnel-up is $connremovestat" >>debugroute.txt
-echo -e "\n" >>debugroute.txt
-echo "# (un)loaded system modules" >>debugroute.txt
-cish -c "system system_modules show" >>debugroute.txt
-echo -e "\n" >>debugroute.txt
-echo "# IPS settings" >>debugroute.txt
-cish -c "show ips-settings" >>debugroute.txt
-echo -e "\n" >>debugroute.txt
-echo "# Firewall General Setting"  >>debugroute.txt
-cish -c "system firewall-acceleration show" >>debugroute.txt
-cish -c "system auto-reboot-on-hang show" >>debugroute.txt
-cish -c "show advanced-firewall" >>debugroute.txt
-echo -e "\n" >>debugroute.txt
-echo "# Version Info" >>debugroute.txt
-cish -c "system diagnostics show version-info" >verinf.txt && cat verinf.txt >>debugroute.txt
-echo -e "\n" >>debugroute.txt
-less debugroute.txt
+echo -e "\n" >quickdebug.txt
+cish -c "show date" >>quickdebug.txt
+echo -e "\n" >>quickdebug.txt
+echo "# Routing and SD-WAN Settings"  >>quickdebug.txt
+cish -c "system route_precedence show" >>quickdebug.txt
+cish -c "show routing sd-wan-policy-route reply-packet" >>quickdebug.txt
+cish -c "show routing sd-wan-policy-route system-generate-traffic" >>quickdebug.txt
+cish -c "show routing reroute-connection" >>quickdebug.txt
+cish -c "show routing reroute-snat-connection" >>quickdebug.txt
+cish -c "show routing source-base-route-for-alias" >>quickdebug.txt
+cish -c "show routing dgd-probe-delay" >>quickdebug.txt
+echo -e "\n" >>quickdebug.txt
+echo "# Settings prone to intermittent network interruptions" >>quickdebug.txt
+connremovestat=$(cish -c "show vpn conn-remove-tunnel-up") && echo "vpn conn-remove-tunnel-up is $connremovestat" >>quickdebug.txt
+echo -e "\n" >>quickdebug.txt
+echo "# (un)loaded system modules" >>quickdebug.txt
+cish -c "system system_modules show" >>quickdebug.txt
+echo -e "\n" >>quickdebug.txt
+echo "# IPS settings" >>quickdebug.txt
+cish -c "show ips-settings" >>quickdebug.txt
+echo -e "\n" >>quickdebug.txt
+echo "# Firewall General Setting"  >>quickdebug.txt
+cish -c "system firewall-acceleration show" >>quickdebug.txt
+cish -c "system auto-reboot-on-hang show" >>quickdebug.txt
+cish -c "show advanced-firewall" >>quickdebug.txt
+echo -e "\n" >>quickdebug.txt
+echo "# Version Info" >>quickdebug.txt
+cish -c "system diagnostics show version-info" >verinf.txt && cat verinf.txt >>quickdebug.txt
+echo -e "\n" >>quickdebug.txt
+less quickdebug.txt
