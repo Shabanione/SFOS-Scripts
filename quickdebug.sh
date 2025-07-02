291b17c99fbfb9ba28a7a92662c2d5102e3a22fc
Shabanione <hirvi@zaphod.ch>
2025-07-02 16:37:30 +0200

    Update quickdebug.sh
    added DHCP lease generation mode check

--- quickdebug.sh
+++ quickdebug.sh
@@ -3,7 +3,7 @@
 # Quick and dirty script to read out some Sophos Firewall settings
 # Should work with every SFOS >V19
 # This script is delivered "as is" - use with care and own risk
-# Last update: 2024-08-14 (added Installation history)
+# Last update: 2025-07-02 (added DHCP lease generation method)
 #
 # Usage: sh ./quickdebug.sh
 #
@@ -22,6 +22,9 @@ echo "# Settings prone to intermittent network interruptions" >>quickdebug.txt
 connremovestat=$(cish -c "show vpn conn-remove-tunnel-up") && echo "vpn conn-remove-tunnel-up is $connremovestat" >>quickdebug.txt
 echo "# (un)loaded system modules" >>quickdebug.txt
 cish -c "system system_modules show" >>quickdebug.txt
+echo "# DHCP Lease Generation.'old' might be troublesome, new appliances have 'global' as default" >>quickdebug.txt
+echo "# Also see Sophos KBA-000004069" >>quickdebug.txt
+cish -c "system dhcp static-entry-scope show" >>quickdebug.txt
 echo "# IPS settings" >>quickdebug.txt
 cish -c "show ips-settings" >>quickdebug.txt
 echo "# Firewall General Setting"  >>quickdebug.txt
